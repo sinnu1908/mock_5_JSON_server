a0dd1c636b6e9f0c6053fdd3f3cd5e765ab18b8e
Shrinivas Katkamwar <sinnu1908@gmail.com>
2023-08-20 18:47:16 +0530

    updated

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const cors=require("cors");
 const connection = require('./db');
 const { empRoutes } = require('./routes/empl.routes');
 const app=express();
+app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => { res.header({"Access-Control-Allow-Origin": "*"}); next(); }) 
-app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "http://localhost:3000/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
+
+
+
 app.use(empRoutes);
 
 
