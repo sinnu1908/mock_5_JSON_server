70c332a0d54445a72968258c25b6df4eabd5da5e
Shrinivas Katkamwar <sinnu1908@gmail.com>
2023-08-20 18:37:55 +0530

    updted

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const cors=require("cors");
 const connection = require('./db');
 const { empRoutes } = require('./routes/empl.routes');
 const app=express();
+app.use(cors());
+
+app.use((req, res, next) => { res.header({"Access-Control-Allow-Origin": "*"}); next(); }) 
 app.use(express.json());
 app.use(empRoutes);
 
-
-app.use(cors());
 
 app.listen(process.env.port||8080,async()=>{
 
